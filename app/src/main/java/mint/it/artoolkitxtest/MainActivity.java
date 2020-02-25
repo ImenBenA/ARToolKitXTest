9e20255fe562612dc78174d51473934005d5a890
ImenBenA <i.benabderrahmen@mintit.io>
2020-02-25 10:46:18 +0100

    First commit

---
+++ app/src/main/java/mint/it/artoolkitxtest/MainActivity.java
@@ -0,0 +1,40 @@
+package mint.it.artoolkitxtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import org.artoolkitx.arx.arxj.ARActivity;
+import org.artoolkitx.arx.arxj.assets.AssetHelper;
+import org.artoolkitx.arx.arxj.rendering.ARRenderer;
+
+public class MainActivity extends ARActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initializeInstance();
+    }
+
+    @Override
+    protected ARRenderer supplyRenderer() {
+        return new ARSimpleRenderer();
+    }
+
+    @Override
+    protected FrameLayout supplyFrameLayout() {
+        return (FrameLayout) this.findViewById(R.id.mainFrameLayout);
+    }
+
+    protected void initializeInstance() {
+
+        // Unpack assets to cache directory so native library can read them.
+        // N.B.: If contents of assets folder changes, be sure to increment the
+        // versionCode integer in the modules build.gradle file.
+        AssetHelper assetHelper = new AssetHelper(getAssets());
+        assetHelper.cacheAssetFolder(this, "Data");
+        assetHelper.cacheAssetFolder(this, "cparam_cache");
+    }
+}
